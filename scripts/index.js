b0c042e539c127cd526af5aa2d77f2d56450124e
Alex Albaran <albaranlex@icloud.com>
2021-04-26 15:11:12 +0200

    Updates

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,35 @@
 const addToList = () => {
   let userData = document.querySelector("#userData").value;
-  //   console.log(userData);
+  
   if (userData != "") {
-    let newLi = document.createElement("li");
+    let newLi = document.createElement("input");
+    newLi.type = "checkbox";
     let text = document.createTextNode(userData);
-    newLi.appendChild(text);
-    document.querySelector(".result").appendChild(newLi);
+    let label = document.createElement("label");
+    let smallCon = document.createElement("div");
+    label.appendChild(text);
+    smallCon.appendChild(newLi);
+    smallCon.appendChild(label);
+    document.querySelector(".result").appendChild(smallCon);
     document.querySelector("#userData").value = "";
+
+    const newContainer = document.createElement("div");
+    const doneButton = document.createElement("button");
+    const deleteButton = document.createElement("button");
+
+    doneButton.innerHTML = "✓";
+    deleteButton.innerHTML = "✗";
+
+    newContainer.appendChild(doneButton);
+    newContainer.appendChild(deleteButton);
+    // Adding class
+    doneButton.classList.add("done");
+    newLi.appendChild(newContainer);
+    const check = () => newLi.classList.toggle("completed");
+    // const del = () => newLi.remove() ;
+    // Adding event listener to my buttons
+    deleteButton.addEventListener("click", () => newLi.remove());
+    doneButton.addEventListener("click", check);
   } else {
     document.querySelector("#userData").placeholder = "Please Enter text first";
   }
