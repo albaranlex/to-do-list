ffd06a0d61beb556e52080b10f80d320a9a431be
Alex Albaran <albaranlex@icloud.com>
2021-04-26 14:47:08 +0200

    element li added

---
+++ src/scripts/index.js
@@ -0,0 +1,13 @@
+const addToList = () => {
+  let userData = document.querySelector("#userData").value;
+  //   console.log(userData);
+  if (userData != "") {
+    let newLi = document.createElement("li");
+    let text = document.createTextNode(userData);
+    newLi.appendChild(text);
+    document.querySelector(".result").appendChild(newLi);
+    document.querySelector("#userData").value = "";
+  } else {
+    document.querySelector("#userData").placeholder = "Please Enter text first";
+  }
+};
