9f7d12fe1fef84d55b2d791091eebc67f6bb06a4
Alex Albaran <albaranlex@icloud.com>
2021-04-26 18:08:02 +0200

    added checkbox

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,30 +1,27 @@
 const addToList = () => {
   let userData = document.querySelector("#userData").value;
   
+  
   if (userData != "") {
-    let newLi = document.createElement("input");
-    newLi.type = "checkbox";
+    let newLi = document.createElement("li");
     let text = document.createTextNode(userData);
-    let label = document.createElement("label");
-    let smallCon = document.createElement("div");
-    label.appendChild(text);
-    smallCon.appendChild(newLi);
-    smallCon.appendChild(label);
-    document.querySelector(".result").appendChild(smallCon);
+    newLi.appendChild(text);
+    document.querySelector(".result").appendChild(newLi);
     document.querySelector("#userData").value = "";
-
-    const newContainer = document.createElement("div");
+    
+    const newContainer = document.createElement("span");
     const doneButton = document.createElement("button");
     const deleteButton = document.createElement("button");
-
+    
     doneButton.innerHTML = "✓";
-    deleteButton.innerHTML = "✗";
-
+    deleteButton.innerHTML = "x";
+    
     newContainer.appendChild(doneButton);
     newContainer.appendChild(deleteButton);
-    // Adding class
+
     doneButton.classList.add("done");
     newLi.appendChild(newContainer);
+
     const check = () => newLi.classList.toggle("completed");
     // const del = () => newLi.remove() ;
     // Adding event listener to my buttons
